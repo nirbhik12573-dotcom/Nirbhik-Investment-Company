c7ed79f063ba12b279a23c1c87aa7047cbd49691
nirbhik12573-dotcom <nirbhik.12573@gems.edu.np>
2026-08-08 11:14:38 +0545

    Correct models indentation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,4 @@
-from django.db import models
-
-    # Create your models here.
-    """
+"""
     Model Fields:
         CharField()
         TextField()
@@ -15,20 +12,27 @@ from django.db import models
         ImageField()
         FileField()
         ForeignKey()
-    """
-    class Department(models.Model):
-        name=models.CharField(max_length=50)
-        
-        def __str__(self):
-            return self.name
+"""    
+    from django.db import models
 
-    class Student(models.Model):
-        name=models.CharField(max_length=50)
-        address=models.TextField(max_length=50)
-        email=models.EmailField(unique=True)
-        age=models.IntegerField()
-        department=models.ForeignKey(Department, on_delete=models.CASCADE)
-        created_at=models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            return self.name    
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField(max_length=50)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
