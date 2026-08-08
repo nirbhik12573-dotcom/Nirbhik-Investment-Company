3dc9bb244a1d407cdf8574f0735a1b8dea69983e
Nirbhik Thapa <nirbhikthapa3@gmail.com>
2026-08-08 15:38:35 +0545

    Configure Neon PostgreSQL database

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,10 +86,10 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
+        conn_max_age=600,
+    )
 }
 
 
